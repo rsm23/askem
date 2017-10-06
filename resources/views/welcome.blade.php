7035d58c62a34983429e8fbf75e554891bcafc95
Rahmani Saif El Moulouk <cool2309@gmail.com>
2017-10-06 13:21:12 +0100

    Fixing Some Typos
    Fixing Some Typos

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,7 +8,7 @@
         <!-- CSRF Token -->
         <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>Laravel {{ app()->version() }}</title>
+        <title>Askem - Q&A Application</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
@@ -103,7 +103,6 @@
 
                 <div class="title m-b-md">
                     Laravel
-                    <p class="versioninfo">Version {{ app()->version() }}</p>
                 </div>
 
                 <div class="links">
